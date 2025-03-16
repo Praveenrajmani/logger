dcfdaad12c67e756d8a49d46f36d988752c7ec04
Praveenrajmani <praveen@min.io>
2025-03-16 18:00:09 +0530

    Add API to read logs

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"sort"
 	"sync"
 	"time"
 
@@ -25,7 +27,7 @@ type Logger[T LogEntry] struct {
 	// current index
 	currentDir            int
 	currentFile           string
-	currentSize           int64
+	currentCompressedSize int64
 	currentFileEntryCount int
 	maxEntriesPerFile     int
 
@@ -41,23 +43,38 @@ type Logger[T LogEntry] struct {
 
 func NewLogger[T LogEntry](dirs []string, batchSize int, maxSize int64, writeInterval time.Duration, writeWorkers int) *Logger[T] {
 	var currentDir int
+	var foundLock bool
+	var currentCompressedSize int64
 	for index, dir := range dirs {
 		lockFilePath := filepath.Join(dir, "dir.lock")
 		if _, err := os.Stat(lockFilePath); err == nil {
 			currentDir = index
+			foundLock = true
+			currentCompressedSize, err = getCompressedSize(dir)
+			if err != nil {
+				panic(err)
+			}
+			break
 		}
 	}
 
 	l := &Logger[T]{
-		dirs:              dirs,
-		batchSize:         batchSize,
-		maxSize:           maxSize,
-		writeInterval:     writeInterval,
-		logChan:           make(chan T, batchSize/2),
-		writeWorkers:      writeWorkers,
-		logBuffer:         make([]T, 0, batchSize*2),
-		maxEntriesPerFile: 100,
-		currentDir:        currentDir,
+		dirs:                  dirs,
+		batchSize:             batchSize,
+		maxSize:               maxSize,
+		writeInterval:         writeInterval,
+		logChan:               make(chan T, batchSize/2),
+		writeWorkers:          writeWorkers,
+		logBuffer:             make([]T, 0, batchSize*2),
+		maxEntriesPerFile:     100,
+		currentDir:            currentDir,
+		currentCompressedSize: currentCompressedSize,
+	}
+
+	if !foundLock {
+		if err := prepareDirectory(l.dirs[l.currentDir]); err != nil {
+			panic(err)
+		}
 	}
 
 	go func() {
@@ -165,33 +182,107 @@ func (l *Logger[T]) compressAndCloseCurrentFile() error {
 	if err != nil {
 		return err
 	}
-	defer writer.Close()
 
 	if _, err := writer.Write(input); err != nil {
+		writer.Close()
 		return err
 	}
+	writer.Close()
 
 	compressedSize := getFileSize(compressedFile)
 	os.Remove(l.currentFile)
 	l.currentFile = ""
 	l.currentFileEntryCount = 0
 
-	l.currentSize += compressedSize
-	if l.currentSize >= l.maxSize {
-		l.closeDirectory(l.dirs[l.currentDir])
+	l.currentCompressedSize += compressedSize
+	if l.currentCompressedSize >= l.maxSize {
+		closeDirectory(l.dirs[l.currentDir])
 		l.currentDir = (l.currentDir + 1) % len(l.dirs)
-		l.currentSize = 0
-		l.prepareDirectory(l.dirs[l.currentDir])
+		l.currentCompressedSize = 0
+		prepareDirectory(l.dirs[l.currentDir])
 	}
 
 	return nil
 }
 
-func (l *Logger[T]) closeDirectory(dir string) {
+func (l *Logger[T]) ReadLogs(limit int) ([]T, error) {
+	l.Lock()
+	defer l.Unlock()
+
+	var logs []T
+
+	currentDir := l.currentDir
+	dirs := make([]string, len(l.dirs))
+	copy(dirs, l.dirs)
+
+	for i := 0; i < len(dirs); i++ {
+		dir := dirs[(currentDir-i+len(dirs))%len(dirs)]
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			continue
+		}
+		sort.Sort(sort.Reverse(byName(entries)))
+
+		for _, entry := range entries {
+			filePath := filepath.Join(dir, entry.Name())
+			var fileLogs []T
+
+			if err := readLogFile(filePath, &fileLogs, limit-len(logs)); err == nil {
+				logs = append(logs, fileLogs...)
+			}
+
+			if len(logs) >= limit {
+				return logs[:limit], nil
+			}
+		}
+	}
+	return logs, nil
+}
+
+type byName []os.DirEntry
+
+func (b byName) Len() int           { return len(b) }
+func (b byName) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b byName) Less(i, j int) bool { return b[i].Name() > b[j].Name() }
+
+func readLogFile[T LogEntry](filePath string, logs *[]T, limit int) error {
+	var reader io.Reader
+	file, err := os.OpenFile(filePath, os.O_RDONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if filepath.Ext(filePath) == ".zst" {
+		zstdReader, err := zstd.NewReader(file)
+		if err != nil {
+			return err
+		}
+		defer zstdReader.Close()
+		reader = zstdReader
+	} else {
+		reader = file
+	}
+
+	decoder := json.NewDecoder(reader)
+	for decoder.More() {
+		if len(*logs) >= limit {
+			break
+		}
+		var log T
+		if err := decoder.Decode(&log); err != nil {
+			return err
+		}
+		*logs = append(*logs, log)
+	}
+	return nil
+}
+
+func closeDirectory(dir string) {
 	os.Remove(filepath.Join(dir, "dir.lock"))
 }
 
-func (l *Logger[T]) prepareDirectory(dir string) error {
+func prepareDirectory(dir string) error {
 	entries, err := os.ReadDir(dir)
 	if err != nil {
 		return err
@@ -206,6 +297,19 @@ func (l *Logger[T]) prepareDirectory(dir string) error {
 		return err
 	}
 	lockFile.Close()
+	return nil
+}
+
+func getCompressedSize(dir string) (size int64, err error) {
+	var entries []os.DirEntry
+	entries, err = os.ReadDir(dir)
+	if err != nil {
+		return
+	}
+	for _, entry := range entries {
+		size += getFileSize(filepath.Join(dir, entry.Name()))
+	}
+	return
 }
 
 func getFileSize(path string) int64 {
@@ -245,6 +349,20 @@ func main() {
 
 		// Respond with success
 		w.WriteHeader(http.StatusOK)
+	})
+
+	http.HandleFunc("/logs", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+		logs, err := logger.ReadLogs(120)
+		if err != nil {
+			http.Error(w, "Failed to read logs", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(logs)
 	})
 
 	port := 8080
